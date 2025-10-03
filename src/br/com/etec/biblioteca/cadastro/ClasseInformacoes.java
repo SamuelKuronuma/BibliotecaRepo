0a4ad58e9d46a1fb57d6c1b0425d73ff11edb229
SamuelKuronuma <samuel.kuronuma@etec.sp.gov.br>
2025-10-03 10:40:12 -0300

    Adicionados getters e setters na classe informações

--- src/br/com/etec/biblioteca/cadastro/ClasseInformacoes.java
+++ src/br/com/etec/biblioteca/cadastro/ClasseInformacoes.java
@@ -11,7 +11,7 @@ public class ClasseInformacoes {
 	//Construtor vazio
 	
 	public ClasseInformacoes(){
-		
+	
 	}
 	
 	//Construtor vazio
@@ -19,5 +19,43 @@ public class ClasseInformacoes {
 	public ClasseInformacoes(String autor, String titulo){
 		this.autoresAt = autor;
 		this.titulosAt = titulo;
+	}
+	
+	//Getters
+	
+	public String getAutores(){
+		return autoresAt;
+	}
+	public String getSubtitulos(){
+		return subtitulosAt;
+	}
+	public int getNumerosPaginas(){
+		return numerosPaginasAt;
+	}
+	public String getGeneros(){
+		return generosAt;
+	}
+	public String getTitulos(){
+		return titulosAt;
+	}
+	
+	//Setters
+	
+	public void setAutores(String autores){
+		this.autoresAt = autores;
+	}
+	
+	public void setSubTitulos(String sub){
+		this.subtitulosAt = sub;
+	}
+	
+	public void setNumerosPaginas(int page){
+		this.numerosPaginasAt = page;
+	}
+	public void setGeneros(String generos){
+		this.generosAt = generos;
+	}
+	public void setTitulos(String titulos){
+		this.titulosAt = titulos;
 	}
 }
