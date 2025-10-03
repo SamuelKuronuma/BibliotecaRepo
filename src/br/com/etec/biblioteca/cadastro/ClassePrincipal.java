6c24a74d6ce808220a8881920f7518d850c37cc4
SamuelKuronuma <samuel.kuronuma@etec.sp.gov.br>
2025-10-03 10:48:59 -0300

    Classe principal alterada

--- src/br/com/etec/biblioteca/cadastro/ClassePrincipal.java
+++ src/br/com/etec/biblioteca/cadastro/ClassePrincipal.java
@@ -3,7 +3,13 @@ package br.com.etec.biblioteca.cadastro;
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ClasseInformacoes Info_obj = new ClasseInformacoes();
+		
+		ClasseInformacoes Info_obj2 = new ClasseInformacoes("Dom Casmurro", "Machado de Assis");
+		
+		Info_obj.mostrarDados();
+		Info_obj2.mostrarDados();
 
 	}
 
