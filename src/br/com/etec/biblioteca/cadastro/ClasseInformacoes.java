e15eb5b9ac6b43fba218978b861ff278de7f9998
SamuelKuronuma <samuel.kuronuma@etec.sp.gov.br>
2025-10-03 10:06:01 -0300

    Adicionadas classe principal e classe informações e construtores

---
+++ src/br/com/etec/biblioteca/cadastro/ClasseInformacoes.java
@@ -0,0 +1,23 @@
+package br.com.etec.biblioteca.cadastro;
+
+public class ClasseInformacoes {
+	
+	String titulosAt;
+	String subtitulosAt;
+	int numerosPaginasAt;
+	String autoresAt;
+	String generosAt;
+	
+	//Construtor vazio
+	
+	public ClasseInformacoes(){
+		
+	}
+	
+	//Construtor vazio
+	
+	public ClasseInformacoes(String autor, String titulo){
+		this.autoresAt = autor;
+		this.titulosAt = titulo;
+	}
+}
